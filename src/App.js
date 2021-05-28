f5e0047d85a3f11ab642fcb6941bb342c02072d4
G0CAR <jovanjacobgomezcarrillo@gmail.com>
2021-05-28 14:52:50 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+import pokeball from './pokeball.svg';
 import './App.css';
+import React,{useContext} from 'react';
+import {ThemeContext} from './Provider/Provider';
+import {theme} from './Theme/theme';
+import ThemeSwitch from './Theme/ThemeSwitch';
+
+const getStyles = (mode)=>({
+  header: {
+    fontSize: 34,
+    fontWeight: "400"
+  },
+  app: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: theme[mode].backgroundColor
+  },
+  text: {
+    fontWeight: "200",
+    color: theme[mode].color
+  },
+  theme: {
+    color: theme[mode].highlight
+  }
+});
 
 function App() {
+  const {mode} = useContext(ThemeContext);
+  const styles = getStyles(mode);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="App" >
+      <header className="App-header" style={styles.app}>
+        <img src={pokeball} className="App-logo" alt="logo" />
+        <p  style={styles.text}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,6 +42,7 @@ function App() {
         >
           Learn React
         </a>
+        <ThemeSwitch />
       </header>
     </div>
   );
---
+++ src/Theme/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+    light: {
+      color: "#202120",
+      backgroundColor: "#ffffff",
+      highlight: "#fdb813"
+    },
+    dark: {
+      color: "#bb86fc",
+      backgroundColor: "#202120",
+      highlight: "#ffffff"
+    }
+  };
+  
